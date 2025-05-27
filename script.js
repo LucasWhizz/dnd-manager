9f05174594d42d8e8aa8d9d148de71861a68cff8
LucasWhizz <luke.oboyle@live.co.uk>
2025-05-27 20:53:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-
+fetch('https://script.google.com/macros/s/YOUR_DEPLOYED_URL_HERE/exec')
+  .then(response => response.json())
+  .then(data => console.log(data)); // Display JSON data in console for now
