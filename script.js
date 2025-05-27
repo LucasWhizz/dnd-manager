da3b13a5dcb96773fb7f9ecb4b42a778514ca86c
LucasWhizz <luke.oboyle@live.co.uk>
2025-05-27 20:58:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 fetch('https://script.google.com/macros/s/YOUR_DEPLOYED_URL_HERE/exec')
   .then(response => response.json())
   .then(data => console.log(data)); // Display JSON data in console for now
+
+function showImage(character) {
+    let img = document.getElementById("char-image");
+
+    const characterImages = {
+        "dargrath": "images/dargrath.jpg",
+        "harvester": "images/harvester.jpg",
+        "neree": "images/neree.jpg",
+        "zeyola": "images/zeyola.jpg",
+        "wildstride": "images/wildstride.jpg",
+        "katara": "images/katara.jpg",
+        "nyanxar": "images/nyanxar.jpg"
+    };
+
+    img.src = characterImages[character];
+    img.style.display = "block";
+}
+
+function hideImage() {
+    let img = document.getElementById("char-image");
+    img.style.display = "none";
+}
